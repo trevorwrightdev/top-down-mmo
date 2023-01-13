40a69376dd2a67e5cd16628a08e0e0133c457e2f
trevorwrightdev <trevor.wright.710@gmail.com>
2023-01-12 22:59:03 -0800

    back to http

--- client/index.js
+++ client/index.js
@@ -1,4 +1,4 @@
-const socket = io('http://rocky-escarpment-91019.herokuapp.com/')
+const socket = io('https://rocky-escarpment-91019.herokuapp.com/')
 
 const Application = PIXI.Application
 let otherPlayers = []
